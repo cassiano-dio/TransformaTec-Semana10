25f081a914d4b480efb059c6d2518b3d76bbaab9
Cassiano Ricardo de Oliveira Peres <97028976+cassiano-dio@users.noreply.github.com>
2022-05-12 12:03:16 -0300

    Delete aula42 directory

--- aula42/tarde/exemplo-4.js
+++
@@ -1,7 +0,0 @@
-const dado = '{"nome":"João", "idade":30, "carro":null}';
-const pessoa = JSON.parse(dado);
-
-for (const prop in pessoa) {
-  console.log("Propriedade: ",pessoa);
-  console.log("Valor: ",pessoa[prop]);
-}
