bb72805fda9f88b9087b3ac26b289fdc5f825288
Cassiano Ricardo de Oliveira Peres <97028976+cassiano-dio@users.noreply.github.com>
2022-05-12 07:38:36 -0300

    Add files via upload

---
+++ aula43/manha/exemplos/exe03.js
@@ -0,0 +1,24 @@
+let a = 10;
+let b = 20;
+
+function multiplicar(a,b){
+    console.log("* =",a*b)
+}
+
+function dividir(a,b){
+    console.log("/ =",a/b)
+}
+
+
+function somar(a,b){
+    console.log("+ =",a+b)
+}
+
+function subtrair(a,b){
+    console.log("- =",a-b)
+}
+
+somar(a,b)
+subtrair(a,b)
+multiplicar(a,b)
+dividir(a,b)
