07920c742c5234ff8d8d753b91102e06e4ce96c2
Cassiano Ricardo de Oliveira Peres <97028976+cassiano-dio@users.noreply.github.com>
2022-05-12 12:06:03 -0300

    Add files via upload

---
+++ aula43/tarde/exe-03.js
@@ -0,0 +1,19 @@
+let n1 = 30
+let n2 = 40
+
+function multiplicar(a,b){
+    console.log("Multiplicação: ", a * b)
+}
+function dividir(a,b){
+    console.log("Divisão: ", a / b)
+}
+function somar(a,b){
+    console.log("Soma: ", a + b)
+}
+function subtrair(a,b){
+    console.log("Subtrair: ", a - b)
+}
+subtrair(n1,n2) //1
+somar(n1,n2) //2
+multiplicar(n1,n2) //3
+dividir(n1,n2) //4
