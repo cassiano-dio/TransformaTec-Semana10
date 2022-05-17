2360cc109383805f77d7d1eaa914bb3d03c7d656
Cassiano Ricardo de Oliveira Peres <97028976+cassiano-dio@users.noreply.github.com>
2022-05-17 07:13:07 -0300

    Delete aula46/aula46 directory

--- aula46/aula46/testes/teste-02.js
+++
@@ -1,8 +0,0 @@
-let x = -1
-
-try {
-    if(b > 0)
-        console.log("Maior que 1")
-} catch (error) {
-        console.log(error)
-}
--- aula46/aula46/testes/teste-05.js
+++
@@ -1,6 +0,0 @@
-const err = new Error ('Erro!')
-try {
-  console.log(f)
-} catch (error) {
-  throw err 
-}
